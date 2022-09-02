a6c3e8bdb395479d41189a8f7a33af9f8e24d393
Antti Viitala <antti.viitala@synpulse.com>
2022-09-02 15:46:27 +0800

    Fix sonarcloud issues

--- dock.py
+++ dock.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
-import sys
 import os
 import re
+import sys
 import time
 
 # Define work apps
@@ -12,7 +12,8 @@ work_apps = [
 
 # Defines desired alias
 alias_name = 'workdock'
-alias_string = """alias {}="python {}\"""".format(alias_name, os.path.realpath(__file__))
+alias_string = """alias {}="python {}\"""".format(
+    alias_name, os.path.realpath(__file__))
 terminal_rc = '.zshrc'
 
 # Some issue with system preferences?
@@ -43,9 +44,8 @@ def append_to_bash_rc(alias):
 
 def get_dock_contents():
     settings = os.popen("defaults read com.apple.dock").read()
-    # bundle_identifier = [x.strip().split("=")[1] for x in settings.split('\n') if 'bundle-identifier' in x]
-    file_labels = [x.strip().split("=")[1][1:-1].replace('"', "") for x in settings.split('\n') if 'file-label' in x]
-    # urlstring = [x.strip().split("=")[1] for x in settings.split('\n') if 'CFURLString' in x]
+    file_labels = [x.strip().split("=")[1][1:-1].replace('"', "")
+                   for x in settings.split('\n') if 'file-label' in x]
 
     output_list = []
     # Resolve the paths - try /Applications/ first, then try /System/Applications
@@ -60,12 +60,15 @@ def get_dock_contents():
 
 
 def send_to_dock(inp, append=True, print_output=False):
-    command = """<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>{}</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>""".format(inp)
+    command = """<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>{}</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>""".format(
+        inp)
     if append:
-        output = "defaults write com.apple.dock persistent-apps -array-add '{}'".format(command)
+        output = "defaults write com.apple.dock persistent-apps -array-add '{}'".format(
+            command)
     else:
         os.system("defaults write com.apple.dock persistent-apps ''")
-        output = "defaults write com.apple.dock persistent-apps -array '{}'".format(command)
+        output = "defaults write com.apple.dock persistent-apps -array '{}'".format(
+            command)
     if print_output:
         print(output)
     time.sleep(0.75)
@@ -89,8 +92,6 @@ if __name__ == "__main__":
         print("Working - adding work apps to the dock...")
         dock_app_list = get_dock_contents()
 
-        # print('Checking if it exists, list below')
-        # print(dock_app_list)
         for app in work_apps:
             # print("\t", app)
             if app not in dock_app_list:
@@ -103,8 +104,6 @@ if __name__ == "__main__":
         # Deleting some icons - Exclude outlook, teams
         app_list = [x for x in dock_app_list if x not in work_apps]
 
-        # print('Checking if it exists, list below')
-        # print(app_list)
         for app in app_list:
             if app == app_list[0]:
                 send_to_dock(app, append=False)
@@ -116,13 +115,15 @@ if __name__ == "__main__":
         try:
             append_to_bash_rc(alias_string)
             print("Alias added succesfully, you can now run 'workdock <command>'. See 'workdock help' for available commands.")
-            print("Please restart your terminal or do 'source ~/.bash_profile' for changes to take effect.")
+            print(
+                "Please restart your terminal or do 'source ~/.bash_profile' for changes to take effect.")
         except IOError:
             print('Permission denied; try editing your .bash_profile manually or run this command again with sudo.')
 
     elif first_arg == 'help':
         print("\nWorkDock - simple command line util to add/delete working apps from the macOS dock.")
-        print("Usage: 'python dock.py <command>', or '{} <command> if alias is set.".format(alias_name))
+        print("Usage: 'python dock.py <command>', or '{} <command> if alias is set.".format(
+            alias_name))
         print("Available commands:")
         print(
             """
@@ -133,7 +134,6 @@ if __name__ == "__main__":
     help \t Prints out this commmands help screen.
             """
         )
-        pass
     else:
         print("\nInvalid arguments. Usage: dock <command>")
         print("To see help text for available commands, you can run:\n\tdock help")
